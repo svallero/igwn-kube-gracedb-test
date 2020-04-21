0d00ac87575ecd773757c54e669ea0d65db824b2
Sara Vallero <svallero@to.infn.it>
2020-04-21 18:02:54 +0200

    Change site name in migrate

--- generate-configmap.sh
+++ generate-configmap.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 kubectl create configmap django-settings-cnaf-test --from-file=configmaps/cnaf_test.py -n gracedb-test
+kubectl create configmap django-update-site --from-file=configmaps/0002_update_site.py -n gracedb-test
